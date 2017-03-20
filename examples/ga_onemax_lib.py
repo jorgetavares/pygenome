34a5a37d653c638fa7b24d9e1d263da55b3b327b
Jorge Tavares <jorge.tavares@outlook.com>
2017-03-21 00:00:15 +0100

    Update example and test for GA onemax library variant

--- examples/ga_onemax_lib.py
+++ examples/ga_onemax_lib.py
@@ -12,16 +12,13 @@ import pygenome.replacement as replacement
 import pygenome.logging as logging
 
 
-# set seed
-np.random.seed(42)
-
 # config GA, minimization fitness function required
 fitness_fn = lambda x : 1. / fitness.onemax(x) 
 onemax_size = 50
 
-# manually coded GA to show case library style
+# manually coded GAs to show case library style
 def genetic_algorithm_binary(fitness_fn, chr_size, pop_size=100, total_generations=20, 
-                             cx_rate=0.7, ind_mt_rate=1.0, op_mt_rate=0.01):
+                             cx_rate=0.7, ind_mt_rate=1.0, op_mt_rate=0.01, elitism=False):
     # initial population
     pop = population.make_integer_population(pop_size, chr_size)
     pop = fitness.evaluate_population(pop, fitness_fn)
@@ -32,22 +29,33 @@ def genetic_algorithm_binary(fitness_fn, chr_size, pop_size=100, total_generatio
         pop = selection.select_population(pop, selection.tournament_selection)
         offsprings = crossover.apply_crossover(pop, cx_rate, crossover.uniform_crossover)
         offsprings = mutation.apply_mutation(offsprings, ind_mt_rate, mutation.flip_mutation, flip_rate=op_mt_rate)
-        pop = replacement.generational(pop, offsprings)
+        if elitism:
+            pop = replacement.elite_strategy(replacement.generational(pop, offsprings), selection.best_individual(pop))
+        else:   
+            pop = replacement.generational(pop, offsprings)
         pop = fitness.evaluate_population(pop, fitness_fn)
         logging.evolution_progress(i, pop)
 
     return pop
 
-# run GA
-def main():
-    # GA execution
+# GA 1
+def generational_no_elitism():
+    np.random.seed(42)
     pop = genetic_algorithm_binary(fitness_fn, onemax_size)
-
-    # do post-run operations
     best = selection.best_individual(pop)
     print('fitness: %s\tgenome: %s' % (best.fitness.value, best.genome))
 
+# GA 2
+def generational_with_elitism():
+    np.random.seed(42)
+    pop = genetic_algorithm_binary(fitness_fn, onemax_size, elitism=True)
+    best = selection.best_individual(pop)
+    print('fitness: %s\tgenome: %s' % (best.fitness.value, best.genome))
 
 # entry point
 if __name__ == "__main__":
-    main()
+    print('GA 1: generational, no elitism')
+    generational_no_elitism()
+
+    print('GA 2: generational, with elitism')
+    generational_with_elitism() 
