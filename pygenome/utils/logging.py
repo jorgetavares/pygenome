87e3321fa3af5955e2fc93b195da05f420769535
Jorge Tavares <jorge.tavares@outlook.com>
2017-03-28 22:56:48 +0200

    New directory structure

---
+++ pygenome/utils/logging.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pygenome as pg
+
+
+def evolution_progress(generation, pop, msg=None):
+    '''
+    Evolution Progress
+
+    Args:
+        generation (int): iteration number
+        pop (Population): population to compute current statistics
+
+    Returns:
+        nothing since it prints basic stats
+    '''
+    samples = [i.fitness.value for i in pop.individuals] # TODO: redo using np.fromiter
+    print('%s\t%s\t%s\t%s' % (generation, np.min(samples), np.mean(samples), np.std(samples)))
