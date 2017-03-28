ece4d4d04bb5cf38577ec3dae68ea1e76204fcce
Jorge Tavares <jorge.tavares@outlook.com>
2017-03-29 00:22:30 +0200

    Add global import to have public root API

--- pygenome/__init__.py
+++ pygenome/__init__.py
@@ -4,17 +4,12 @@ PyGenome - Evolutionary Algorithms in Python
 
 __version__ = 0.01
 
-from pygenome.representations import individual
-from pygenome.representations import population
-
-from pygenome.fitness import evaluation
-
-from pygenome.operators import crossover
-from pygenome.operators import mutation
-from pygenome.operators import selection
-from pygenome.operators import replacement
-
-from pygenome.utilities import logging
-
-from pygenome.engines import algorithms
-
+from .representations.individual import *
+from .representations.population import *
+from .fitness.evaluation import *
+from .operators.crossover import *
+from .operators.mutation import *
+from .operators.selection import *
+from .operators.replacement import *
+from .utilities.logging import *
+from .engines.algorithms import *
