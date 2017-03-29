6c4cfe4019e1d125cf1911ae1f6984e5ec9613bb
Jorge Tavares <jorge.tavares@microsoft.com>
2017-03-29 16:50:40 +0200

    Add base file for tree-based representations (GP)

---
+++ pygenome/representations/tree.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pygenome as pg
+import operators as ops
+
+# TODO:
+# create function + terminal sets
+
+# create tree initializers
+
+# -> operators
+
+# -> eval
+
+# -> example
+
