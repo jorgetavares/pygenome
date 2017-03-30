db9c7b752ea2877e8c3fd2cda23c29edc967af66
Jorge Tavares <jorge.tavares@outlook.com>
2017-03-31 00:29:16 +0200

    Fix linear tree interpreter

--- pygenome/representations/tree.py
+++ pygenome/representations/tree.py
@@ -1,13 +1,13 @@
 import numpy as np
-import pygenome as pg
+#import pygenome as pg
 import operator as op
 
 # container for the functiona and terminal set
 class PrimitiveSet(object):
 
     def __init__(self, typed=False):
-        self.functions = None
-        self.terminals = None
+        self.functions = {}
+        self.terminals = {}
         self.num_primitives = 0
         self.typed = typed
 
@@ -16,7 +16,7 @@ class PrimitiveSet(object):
         if self.typed and types is None:
             raise AttributeError('This is a typed primitive set so types are required!')
 
-        if typed:
+        if self.typed:
             primitive = (fn, arity, types)
         else:
             primitive = (fn, arity)
@@ -29,22 +29,20 @@ class PrimitiveSet(object):
         if self.typed and types is None:
             raise AttributeError('This is a typed primitive set so types are required!')
         
-        if typed:
+        if self.typed:
             primitive = (term, 0, types)
         else:
             primitive = (term, 0)
 
         self.num_primitives += 1
-        self.terminals[num_primitives] = primitive
+        self.terminals[self.num_primitives] = primitive
 
 
-def run-linear-tree(pset, tree):
+def run_linear_tree(pset, tree):
 
-    position = 0
-
-    def run-tree(array_tree):
-        element = array_tree[position]
-        position +=1
+    def run_tree(array_tree):
+        element = array_tree[run_tree.position]
+        run_tree.position +=1
 
         if element in pset.terminals:
             terminal, _ = pset.terminals[element]
@@ -53,12 +51,14 @@ def run-linear-tree(pset, tree):
             fn, arity = pset.functions[element]
             args = []
             for i in range(arity):
-                args.append(run-tree(array_tree))
-            return getattr(fn)(*args)
+                args.append(run_tree(array_tree))
+            return fn(*args)
         else:
             raise AttributeError('Primitive not found in Primitive Set!')
-
-    run-tree(tree)
+    
+    run_tree.position = 0
+    result = run_tree(tree)
+    return result
 
 
 # TODO:
