b13cdc8fec6c7f9adc0169d3e5edad54c475fb10
Jorge Tavares <jorge.tavares@microsoft.com>
2017-03-30 14:42:43 +0200

    Add PrimitiveSet

--- pygenome/representations/tree.py
+++ pygenome/representations/tree.py
@@ -2,6 +2,31 @@ import numpy as np
 import pygenome as pg
 import operators as ops
 
+# container for the functiona and terminal set
+class PrimitiveSet(object):
+
+    def __init__(self, typed=False):
+        self.functions = None
+        self.terminals = None
+        self.typed = typed
+
+
+    def add_function(self, fn, arity, types=None):
+        if self.typed and types is None:
+            raise AttributeError('This is a typed primitive set so types are required!')
+
+        if typed:
+            primitive = (fn, arity, types)
+        else:
+            primitive = (fn, arity)
+
+        self.functions.append(primitive) 
+
+
+    def add_terminal(self, term):
+        self.terminals.append(term)
+
+
 # TODO:
 # create function + terminal sets
 
