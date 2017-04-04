7396edd1fcade8ff64c728b8b28f29826d9c6c5c
Jorge Tavares <jorge.tavares@outlook.com>
2017-04-04 23:40:25 +0200

    Remove duplicate fitness functions

--- pygenome/fitness/evaluation.py
+++ pygenome/fitness/evaluation.py
@@ -28,31 +28,3 @@ def evaluate_population(pop, fitness_fn, **kargs):
         ind.fitness = Fitness(fitness_fn(ind.genome, **kargs))
     
     return pop
-
-
-def onemax(vector):
-    '''
-    Onemax fitness function
-
-    Args:
-        vector (array): binary array
-    
-    Returns:
-        sum of ones in the binary array
-    '''
-    return np.sum(vector)
-
-
-def sphere_model(vector):
-    '''
-    Sphere Model fitness function
-
-    Args:
-        vector (array): float array
-    
-    Returns:
-        computed solution
-    '''
-    sphere = np.vectorize(lambda x : x ** 2.0)
-
-    return np.sum(sphere(vector))
