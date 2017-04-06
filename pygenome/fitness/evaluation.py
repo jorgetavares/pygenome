e194382866b648c59706046fa84c03a3f0dfd6b5
Jorge Tavares <jorge.tavares@microsoft.com>
2017-04-06 14:04:56 +0200

    Add evaluate_tree_population

--- pygenome/fitness/evaluation.py
+++ pygenome/fitness/evaluation.py
@@ -28,3 +28,26 @@ def evaluate_population(pop, fitness_fn, **kargs):
         ind.fitness = Fitness(fitness_fn(ind.genome, **kargs))
     
     return pop
+
+
+def evaluate_tree_population(pop, fitness_fn, pset, **kargs):
+    '''
+    Evaluate Tree Population
+
+    Args:
+        pop (Population): population to be evaluated in-place
+        fitness_fn (function): fitness function that receives an individual
+        pset (PrimitiveSet): set of primitives
+        **kargs: keyword arguments that fitness_fn might have
+
+    Returns:
+        evaluated population by calling the fitness function
+    '''
+    for i in range(pop.size):
+        ind = pop.individuals[i]
+        ind.fitness = Fitness(fitness_fn(ind.genome, **kargs))
+        depth, nodes = pg.count_tree_internals(pset, ind.genome)
+        ind.depth = depth
+        ind.nodes = nodes
+
+    return pop
