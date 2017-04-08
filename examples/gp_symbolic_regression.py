653f507e9fcf778a9602b25ab47e8563a0fb71d0
Jorge Tavares <jorge.tavares@outlook.com>
2017-04-08 22:42:36 +0200

    Add GP symbolic regression example

--- examples/gp_symbolic_regression.py
+++ examples/gp_symbolic_regression.py
@@ -4,9 +4,56 @@ sys.path.append('../')
 import numpy as np
 import pygenome as pg
 
+
+# define Primitive Set 
+import operator as op
+
+def protected_div(a, b):
+    if b > 0:
+        return op.truediv(a, b)
+    else:
+        return b
+
+pset = pg.PrimitiveSet()
+# functions
+pset.addFunction(op.add, 2)
+pset.addFunction(op.sub, 2)
+pset.addFunction(op.mul, 2)
+pset.addFunction(protected_div, 2)
+# constants
+num_constants = 10
+for i in range(num_constants):
+    pset.addTerminal(np.random.randint(-5, 5))
+# variables
+pset.addVariable("x")
+
+
 # define problem
-sphere_model_size = 30
-fitness_fn = pg.sphere_model
+# y = f(x) = x^2 / 2
+def fn(x):
+    return protected_div(op.mul(x, x), 2.0)
+
+from math import sqrt
+
+def fitness_regression():
+    num_fitness_cases = 10
+    x_points = [x for x in range(num_fitness_cases)]
+    y_points = [fn(x) for x in x_points]
+    
+    def regression(solution):
+        vars_inputs = {}
+        total = 0
+        for i in range(num_fitness_cases)
+            vars_inputs["x"] = x_points[i]
+            result = pg.interpreter(pset, solution, run=True, vars_inputs=vars_inputs)
+            total += sqrt(result ** 2 - y_points[i] ** 2)
+        
+        return total
+
+    return regression
+
+fitness_fn = fitness_regression()
+
 
 # GP
 def gp_with_elitism():
