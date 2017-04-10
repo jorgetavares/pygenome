a1ee27935f4ded4590d36a331a8208d51cf41641
Jorge Tavares <jorge.tavares@microsoft.com>
2017-04-10 12:12:42 +0200

    Add documentation to MSR

--- pygenome/fitness/loss.py
+++ pygenome/fitness/loss.py
@@ -2,4 +2,14 @@ import numpy as np
 
 
 def mean_squared_error(x, y):
+    '''
+    Mean Squared Error (MSR)
+
+    Args:
+        x (array): array of predictions
+        y (array): array of labels
+
+    Returns:
+        the mean squared error
+    '''
     return np.sum(np.sqrt(np.square(x) - np.square(y)))
