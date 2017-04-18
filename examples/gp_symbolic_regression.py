a094e1c979bf335bf200d2456dbb193e3f57ba37
Jorge Tavares <jorge.tavares@outlook.com>
2017-04-18 21:47:20 +0200

    Make GP example deterministic (and update test)

--- examples/gp_symbolic_regression.py
+++ examples/gp_symbolic_regression.py
@@ -14,36 +14,42 @@ def protected_div(a, b):
     else:
         return b
 
-pset = pg.PrimitiveSet()
-# functions
-pset.addFunction(op.add, 2)
-pset.addFunction(op.sub, 2)
-pset.addFunction(op.mul, 2)
-pset.addFunction(protected_div, 2)
-# constants
-num_constants = 10
-for i in range(num_constants):
-    pset.addTerminal(np.random.randint(-5, 5))
-# variables
-pset.addVariable("x")
+def setup_primitive_set():
+    pset = pg.PrimitiveSet()
+    # functions
+    pset.addFunction(op.add, 2)
+    pset.addFunction(op.sub, 2)
+    pset.addFunction(op.mul, 2)
+    pset.addFunction(protected_div, 2)
+    # constants
+    num_constants = 10
+    for i in range(num_constants):
+        pset.addTerminal(np.random.randint(-5, 5))
+    # variables
+    pset.addVariable("x")
 
+    return pset
 
 # define problem
 # y = f(x) = x^2 / 2
 def fn(x):
     return protected_div(op.mul(x, x), 2.0)
 
-num_fitness_cases = 10
-fitness_fn = pg.make_fitness_regression(pset, fn, num_fitness_cases)
-
-
 # GP
 def gp_with_elitism():
     np.random.seed(42)
+    
     min_depth = 1
     max_depth = 4
-    max_size = 200
+    max_size = 100
+    
+    pset = setup_primitive_set()
+    
+    num_fitness_cases = 10
+    fitness_fn = pg.make_fitness_regression(pset, fn, num_fitness_cases)
+    
     pop = pg.genetic_programming(fitness_fn, pset, min_depth, max_depth, max_size, elitism=True, total_generations=20, pop_size=1000)
+    
     best = pg.best_individual(pop)
     print('fitness: %s\tgenome: %s' % (best.fitness.value, best.genotype))
 
