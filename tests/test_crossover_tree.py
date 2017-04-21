5508297e3a2ae90dc5681312424c12920e2c1caa
Jorge Tavares <jorge.tavares@microsoft.com>
2017-04-21 12:06:14 +0200

    Add tests for typed tree crossover

--- tests/test_crossover_tree.py
+++ tests/test_crossover_tree.py
@@ -6,6 +6,13 @@ import pygenome as pg
 import operator as op
 
 
+def protected_div(a, b):
+    if b > 0:
+        return op.truediv(a, b)
+    else:
+        return b
+
+
 def test_tree_crossover1():
     np.random.seed(42)
     pset = pg.PrimitiveSet()
@@ -37,15 +44,7 @@ def test_tree_crossover1():
     assert i2m_str == '3'
 
 def test_tree_crossover2():
-
-    def protected_div(a, b):
-        if b > 0:
-            return op.truediv(a, b)
-        else:
-            return b
-
     np.random.seed(42)
-
     pset = pg.PrimitiveSet()
     pset.addFunction(op.add, 2)
     pset.addFunction(op.sub, 2)
@@ -73,3 +72,71 @@ def test_tree_crossover2():
     assert o2.nodes == 3
     assert np.array_equal(o2.genotype, np.array([1, 5, 7, 0, 0, 0, 0, 0, 0, 0]))
     assert o2_str == 'add(1, 2)'
+
+
+def test_tree_crossover_typed1():
+    np.random.seed(42)
+    pset = pg.PrimitiveSet(typed=True)
+    pset.addFunction(op.add, 2, types=[int, int, int])
+    pset.addFunction(op.sub, 2, types=[int, int, int])
+    pset.addFunction(op.mul, 2, types=[int, int, int])
+    pset.addFunction(protected_div, 2, types=[float, float, float])
+    num_constants = 5
+    for i in range(num_constants):
+        pset.addTerminal(np.random.randint(-5, 5), types=[int])
+    for i in range(num_constants):
+        pset.addTerminal(np.random.uniform(), types=[float])
+    pset.addVariable("x", types=[int])
+
+    pop = pg.make_tree_population(2, pset, 1, 4, 50, init_method=pg.full_tree)
+    o1, o2 = pg.tree_crossover(pop.individuals[0], pop.individuals[1], pset=pset)
+    o1_str = pg.interpreter(pset, o1.genotype)
+    o2_str = pg.interpreter(pset, o2.genotype)
+
+    assert o1.depth == 6
+    assert o1.nodes == 21
+    assert np.array_equal(o1.genotype, np.array([ 3,  2,  1,  6,  8,  2,  1,  3,  7,  6,  3,  7,  8,  8,  1,  1,  8,
+        6,  2, 14,  8,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]))
+    assert o1_str == 'mul(sub(add(-2, -1), sub(add(mul(2, -2), mul(2, -1)), -1)), add(add(-1, -2), sub(x, -1)))'
+    
+    assert o2.depth == 4
+    assert o2.nodes == 9
+    assert np.array_equal(o2.genotype, np.array([ 1,  6,  3,  1,  7, 14,  3, 14,  5,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]))
+    assert o2_str == 'add(-2, mul(add(2, x), mul(x, 1)))'
+
+def test_tree_crossover_typed2():
+    np.random.seed(42)
+    pset = pg.PrimitiveSet(typed=True)
+    pset.addFunction(op.add, 2, types=[int, int, int])
+    pset.addFunction(op.sub, 2, types=[int, float, float])
+    pset.addFunction(op.mul, 2, types=[float, int, int])
+    pset.addFunction(protected_div, 2, types=[float, float, float])
+    num_constants = 5
+    for i in range(num_constants):
+        pset.addTerminal(np.random.randint(-5, 5), types=[int])
+
+    for i in range(num_constants):
+        pset.addTerminal(np.random.uniform(), types=[float])
+    pset.addVariable("x", types=[int])
+
+    pop = pg.make_tree_population(2, pset, 1, 4, 50, init_method=pg.full_tree)
+    o1, o2 = pg.tree_crossover(pop.individuals[0], pop.individuals[1], pset=pset)
+    o1_str = pg.interpreter(pset, o1.genotype)
+    o2_str = pg.interpreter(pset, o2.genotype)
+
+    assert o1.depth == 4
+    assert o1.nodes == 13
+    assert np.array_equal(o1.genotype, np.array([ 3,  2, 12,  4, 10, 12,  1,  1,  8,  6,  2, 13, 12,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]))
+    assert o1_str == 'mul(sub(0.33370861113902184, protected_div(0.09997491581800289, 0.33370861113902184)), add(add(-1, -2), sub(0.14286681792194078, 0.33370861113902184)))'
+    
+    assert o2.depth == 5
+    assert o2.nodes == 17
+    assert np.array_equal(o2.genotype, np.array([ 1,  1,  1,  7,  6,  2,  3,  6,  8, 12,  2,  3,  7,  8,  3,  8,  5,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]))
+    assert o2_str == 'add(add(add(2, -2), sub(mul(-2, -1), 0.33370861113902184)), sub(mul(2, -1), mul(-1, 1)))'
