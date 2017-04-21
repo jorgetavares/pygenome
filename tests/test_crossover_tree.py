bc80e312757026ac327bd1ee6e721f1d0e50d23e
Jorge Tavares <jorge.tavares@microsoft.com>
2017-04-21 10:28:20 +0200

    Add extra test for tree crossover

--- tests/test_crossover_tree.py
+++ tests/test_crossover_tree.py
@@ -6,7 +6,7 @@ import pygenome as pg
 import operator as op
 
 
-def test_tree_crossover():
+def test_tree_crossover1():
     np.random.seed(42)
     pset = pg.PrimitiveSet()
     pset.addFunction(op.add, 2)
@@ -35,4 +35,41 @@ def test_tree_crossover():
        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
        0, 0, 0, 0]))
     assert i2m_str == '3'
+
+def test_tree_crossover2():
+
+    def protected_div(a, b):
+        if b > 0:
+            return op.truediv(a, b)
+        else:
+            return b
+
+    np.random.seed(42)
+
+    pset = pg.PrimitiveSet()
+    pset.addFunction(op.add, 2)
+    pset.addFunction(op.sub, 2)
+    pset.addFunction(op.mul, 2)
+    pset.addFunction(protected_div, 2)
+    num_constants = 10
+    for i in range(num_constants):
+        pset.addTerminal(np.random.randint(-5, 5))
+    pset.addVariable("x")
     
+    t1 = np.array([3, 9, 1, 1, 5, 7, 6, 0, 0, 0])
+    t2 = np.array([6, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    i1 = pg.TreeIndividual(tree=t1, nodes=7)
+    i2 = pg.TreeIndividual(tree=t2, nodes=1)
+    o1, o2 = pg.tree_crossover(i1, i2, pset=pset)
+    o1_str = pg.interpreter(pset, o1.genotype)
+    o2_str = pg.interpreter(pset, o2.genotype)
+    
+    assert o1.depth == 3
+    assert o1.nodes == 5
+    assert np.array_equal(o1.genotype, np.array([3, 9, 1, 6, 6, 0, 0, 0, 0, 0]))
+    assert o1_str == 'mul(4, add(-2, -2))'
+    
+    assert o2.depth == 2
+    assert o2.nodes == 3
+    assert np.array_equal(o2.genotype, np.array([1, 5, 7, 0, 0, 0, 0, 0, 0, 0]))
+    assert o2_str == 'add(1, 2)'
