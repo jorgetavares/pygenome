9264f03ce11f8b9b4bbb232ce02485d8575e2eb3
Jorge Tavares <jorge.tavares@outlook.com>
2017-04-24 23:15:27 +0200

    Add test for apply global crossover

--- tests/test_crossover.py
+++ tests/test_crossover.py
@@ -54,4 +54,14 @@ def test_partially_match_crossover():
     o1, o2 = pg.partially_match_crossover(i1, i2)
     assert np.array_equal(o1.genotype, np.array([4, 7, 8, 3, 6, 5, 1, 2, 0, 9])) 
     assert np.array_equal(o2.genotype, np.array([9, 3, 6, 7, 8, 1, 5, 4, 2, 0])) 
-    
+
+def test_apply_global_crossover():
+    np.random.seed(42)
+    size = 1000
+    ind_size = 100
+    operator = pg.intermediary_crossover
+    pop = pg.make_uniform_population(size, ind_size)
+    original_pop = pop.clone()
+    pop = pg.apply_global_crossover(pop, operator)
+    for i in range(pop.size):
+        assert pop.individuals[i].run_eval == True
