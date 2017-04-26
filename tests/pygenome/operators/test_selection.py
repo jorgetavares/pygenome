702ad3b756d73e9f344506c6e5c77dce5f05b090
Jorge Tavares <jorge.tavares@microsoft.com>
2017-04-26 14:51:45 +0200

    Add tests for population selects

--- tests/pygenome/operators/test_selection.py
+++ tests/pygenome/operators/test_selection.py
@@ -1,6 +1,39 @@
 import numpy as np
 import pygenome as pg
 
+def test_select_population():
+    np.random.seed(42)
+    pop = pg.make_integer_population(10, 5)
+    pop = pg.evaluate_population(pop, lambda x : 1. / (1. + pg.onemax(x)))
+    diff = 0
+    for i in range(pop.size):
+        if not np.array_equal(pop.individuals[i].genotype, pop.individuals[i].genotype):
+            diff +=1
+    assert diff == 0
+
+    original_pop = pop.clone()
+    pop = pg.select_population(pop, pg.tournament_selection)
+    for i in range(pop.size):
+        if not np.array_equal(pop.individuals[i].genotype, original_pop.individuals[i].genotype):
+            diff +=1
+    assert diff != 0
+
+def test_select_steadystate_population():
+    np.random.seed(42)
+    pop = pg.make_integer_population(10, 5)
+    pop = pg.evaluate_population(pop, lambda x : 1. / (1. + pg.onemax(x)))
+    diff = 0
+    for i in range(pop.size):
+        if not np.array_equal(pop.individuals[i].genotype, pop.individuals[i].genotype):
+            diff +=1
+    assert diff == 0
+
+    original_pop = pop.clone()
+    pop = pg.select_steadystate_population(pop, pg.tournament_selection)
+    for i in range(pop.size):
+        if not np.array_equal(pop.individuals[i].genotype, original_pop.individuals[i].genotype):
+            diff +=1
+    assert diff != 0
 
 def test_best_individual():
     np.random.seed(42)
