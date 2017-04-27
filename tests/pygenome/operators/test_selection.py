9c758d4735342803b2c0e8c2bd7ecaf71f07db39
Jorge Tavares <jorge.tavares@microsoft.com>
2017-04-27 14:45:36 +0200

    Add tests for selection operators

--- tests/pygenome/operators/test_selection.py
+++ tests/pygenome/operators/test_selection.py
@@ -50,3 +50,52 @@ def test_worst_individual():
     worst = pg.worst_individual(pop)
     assert np.array_equal(worst.genotype, np.array([0, 1, 0, 0, 0]))
     assert worst.fitness.value == 0.5
+
+def test_random_selection():
+    np.random.seed(42)
+    individuals = [pg.Individual(fitness=pg.Fitness(value=np.random.randint(10))) for _ in range(10)]    
+    pop = pg.Population(individuals=np.array(individuals))
+    # [6, 3, 7, 4, 6, 9, 2, 6, 7, 4]
+    np.random.seed(42)
+    inds1 = [pg.random_selection(pop) for _ in range(10)]
+    assert [inds1[i].fitness.value for i in range(10)] == [2, 4, 6, 6, 2, 4, 7, 2, 6, 6]
+    np.random.seed(24)
+    inds2 = [pg.random_selection(pop) for _ in range(10)]
+    assert [inds2[i].fitness.value for i in range(10)] == [7, 4, 6, 6, 3, 3, 3, 6, 6, 4]
+    
+def test_tournament_selection():
+    np.random.seed(42)
+    individuals = [pg.Individual(fitness=pg.Fitness(value=np.random.randint(10))) for _ in range(10)]    
+    pop = pg.Population(individuals=np.array(individuals))
+    # [6, 3, 7, 4, 6, 9, 2, 6, 7, 4]
+    np.random.seed(42)
+    inds1 = [pg.tournament_selection(pop, size=5) for _ in range(10)]
+    assert [inds1[i].fitness.value for i in range(10)] == [2, 2, 4, 6, 3, 4, 4, 2, 2, 2]
+    np.random.seed(24)
+    inds2 = [pg.tournament_selection(pop, size=5) for _ in range(10)]
+    assert [inds2[i].fitness.value for i in range(10)] == [4, 3, 4, 4, 4, 3, 2, 2, 3, 3]
+ 
+def test_negative_tournament_selection():
+    np.random.seed(42)
+    individuals = [pg.Individual(fitness=pg.Fitness(value=np.random.randint(10))) for _ in range(10)]    
+    pop = pg.Population(individuals=np.array(individuals))
+    # [6, 3, 7, 4, 6, 9, 2, 6, 7, 4]
+    np.random.seed(42)
+    inds1 = [pg.negative_tournament_selection(pop, size=5) for _ in range(10)]
+    assert [inds1[i].fitness.value for i in range(10)] == [6, 7, 6, 9, 9, 9, 7, 7, 7, 7]
+    np.random.seed(24)
+    inds2 = [pg.negative_tournament_selection(pop, size=5) for _ in range(10)]
+    assert [inds2[i].fitness.value for i in range(10)] == [7, 6, 7, 7, 6, 6, 3, 4, 6, 7]
+
+def test_roulette_wheel_selection():
+    np.random.seed(42)
+    individuals = [pg.Individual(fitness=pg.Fitness(value=np.random.randint(10))) for _ in range(10)]    
+    pop = pg.Population(individuals=np.array(individuals))
+    # [6, 3, 7, 4, 6, 9, 2, 6, 7, 4]
+    np.random.seed(42)
+    inds1 = [pg.roulette_wheel_selection(pop) for _ in range(10)]
+    assert [inds1[i].fitness.value for i in range(10)] == [4, 4, 2, 2, 3, 3, 6, 7, 2, 2]
+    np.random.seed(24)
+    inds2 = [pg.roulette_wheel_selection(pop) for _ in range(10)]
+    assert [inds2[i].fitness.value for i in range(10)] == [4, 2, 4, 3, 4, 6, 4, 4, 3, 4]
+ 
