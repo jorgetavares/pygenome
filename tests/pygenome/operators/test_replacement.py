e47358ddf5a7742fcb36b5b6ab34a5b7b096f37d
Jorge Tavares <jorge.tavares@microsoft.com>
2017-04-27 18:19:35 +0200

    Add test for steady state replacement

--- tests/pygenome/operators/test_replacement.py
+++ tests/pygenome/operators/test_replacement.py
@@ -12,4 +12,24 @@ def test_generational():
     assert pop1.equal(pop2) is True
     assert pop1.equal(pop1_clone) is False
 
+# this tests needs to be improved
+def test_steady_state():
+    np.random.seed(42)
+    size1 = 10
+    ind_size = 20
+    pop1 = pg.make_integer_population(size1, ind_size)
+    pop1_clone = pop1.clone()
+    size2 = 2
+    pop2 = pg.make_integer_population(size2, ind_size)
+    pop1 = pg.steady_state_replacement(pop1, pop2)
+    found1 = False
+    found2 = False
+    for ind in pop1.individuals:
+        if ind.equal(pop2.individuals[0]) is True:
+            found1 = True
+        if ind.equal(pop2.individuals[1]) is True:
+            found2 = True
+        if found1 and found2:
+            break
 
+    assert found1 and found2 is True
