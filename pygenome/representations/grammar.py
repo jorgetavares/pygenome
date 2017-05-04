8090828a9abc6bc9e8f366907240a26492d0c2a6
Jorge Tavares <jorge.tavares@microsoft.com>
2017-05-04 15:50:39 +0200

    Refactor grammar parser. Add start symbol

--- pygenome/representations/grammar.py
+++ pygenome/representations/grammar.py
@@ -5,18 +5,29 @@ class Grammar(object):
 
     def __init__(self, filename=None):
         if filename is not None:
-            self.grammar = self.parseFromFile(filename)
+            self.grammar, self.start_symbol = self.parseFromFile(filename)
         else:
             self.grammar = {}
+            self.start_symbol = None
 
     def parseFromFile(self, filename):
+        
+        def parse_line(line):
+            rule = line.split(sep=':=')
+            left_side = rule[0].strip()
+            right_side = [x.strip() for x in rule[1].split(sep='|')]
+            return left_side, right_side
+
         grammar = {}
         
         with open(filename, 'r') as input_file:
+            left_side, right_side = parse_line(input_file.readline())
+            grammar[right_side] = left_side
+            start_symbol = left_side
+
             for line in input_file:
-                rule = line.split(sep=':=')
-                right_side = rule[0].strip()
-                left_side = [x.strip() for x in rule[1].split(sep='|')]
+                left_side, right_side = parse_line(line)
                 grammar[right_side] = left_side
         
-        return grammar
+        return grammar, start_symbol
+    
