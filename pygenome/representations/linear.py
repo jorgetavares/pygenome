9c32323c5b491965f5ba46aea734371678cd3834
Jorge Tavares <jorge.tavares@microsoft.com>
2017-05-05 12:01:35 +0200

    PEP8 format

--- pygenome/representations/linear.py
+++ pygenome/representations/linear.py
@@ -9,13 +9,13 @@ def integer_chromossome(size, low=0, high=1):
         size (int): the fixed size of the chromossome
         min_value (int): minimum value that can be in the chromossome
         max_value (int): maximum value (inclusive) that can be in the chromossome
-    
+
     Returns:
         fixed size chromossome initialized with random integers
     '''
     high_value = high + 1
     chromossome = np.random.randint(low, high=high_value, size=size)
-    
+
     return chromossome
 
 
@@ -25,7 +25,7 @@ def permutation_chromossome(size):
 
     Args:
         size (int): the fixed size of the chromossome
-    
+
     Returns:
         fixed size chromossome initialized with a random permutation from 0 to size -1 
     '''
@@ -40,12 +40,12 @@ def uniform_chromossome(size, low=0.0, high=1.0):
         size (int): the fixed size of the chromossome
         min_value (float): minimum value that can be in the chromossome
         max_value (float): maximum value (exclusive) that can be in the chromossome
-    
+
     Returns:
         fixed size chromossome initialized with random floats using a uniform distribution
     '''
     return np.random.uniform(low=low, high=high, size=size)
-    
+
 
 def normal_chromossome(size, mean=0.0, sigma=1.0):
     '''
@@ -55,7 +55,7 @@ def normal_chromossome(size, mean=0.0, sigma=1.0):
         size (int): the fixed size of the chromossome
         mean (float): mean of the normal distribution
         sigma (float): sigma of the normal distribution
-    
+
     Returns:
         fixed size chromossome initialized with random floats using a normal distribution
     '''
