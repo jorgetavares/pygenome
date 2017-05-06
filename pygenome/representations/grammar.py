5cd97e1a2019696aaeea1248400ea09d7547df51
Jorge Tavares <jorge.tavares@outlook.com>
2017-05-06 23:39:28 +0200

    Fix randomDerivative

--- pygenome/representations/grammar.py
+++ pygenome/representations/grammar.py
@@ -32,15 +32,19 @@ class Grammar(object):
 
         return grammar, start_symbol
 
-    def randomDerivative(self, current_symbol):
+    def randomDerivative(self, current_symbol, isproductions=False):
+        
         if current_symbol in self.grammar:
             productions = self.grammar[current_symbol]
 
             if type(productions) is list:
                 size = len(productions)
-                return self.randomDerivative(productions[np.random.randint(size)])
+                return self.randomDerivative(productions[np.random.randint(size)], isproductions=True)
             else:
                 symbols = productions.split()
-                return " ".join([self.andomDerivative(symbol) for symbol in productions])
+                return " ".join([self.randomDerivative(symbol) for symbol in symbols], isproductions=True)
+        elif isproductions is True:
+            symbols = current_symbol.split()
+            return " ".join([self.randomDerivative(symbol) for symbol in symbols])
         else:
             return current_symbol
